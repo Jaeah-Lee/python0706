a35b8f232a4eade109a58fe78fbd101ded1e18ff
Jaeah-Lee <jaeah2171@gmail.com>
2023-07-06 17:26:26 +0900

    230706

---
+++ basic.py
@@ -0,0 +1,15 @@
+name = input("이름 입력 : ")
+print(name)
+
+try :
+    #문자열을 정수로 변환
+    age = int(input("나이 입력 : "))
+    print(age - 1)
+except :
+    print("문제 발생")
+
+# 여러 개 입력
+hobbys = input("취미를 ,로 구분해서 입력 : ").split(",")
+for hobby in hobbys:
+    print(hobby)
+print("프로그램 종료")
